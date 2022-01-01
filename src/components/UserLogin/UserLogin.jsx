d57615e9d5b284ba8cabbd9c9521892e789f71c2
Andrey Codez <andreycodez@bixtrix.com>
2022-01-02 03:39:34 +1000

    Login page UI done

--- src/components/UserLogin/UserLogin.jsx
+++ src/components/UserLogin/UserLogin.jsx
@@ -1,6 +1,19 @@
+import {Link} from "react-router-dom";
+
 const UserLogin = () => {
   return(
-      <>UserLogin page</>
+      <div className="center-display">
+        <form className="form">
+          <label htmlFor="credentials">Enter you <strong>email address</strong> and <strong>password</strong></label>
+          <input id="credentials" type="email" className="form-control" name="email"/>
+          <input id="credentials" type="password" className="form-control" name="password" />
+          <input type="submit" className="submit-btn" value="Sign in" />
+        </form>
+        <div className="footer-text">
+          No account? Create one&nbsp;
+          <Link to="/register">Here</Link>
+        </div>
+      </div>
   )
 }
 
