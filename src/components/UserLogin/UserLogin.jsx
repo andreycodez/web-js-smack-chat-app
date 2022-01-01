ca4960e1650b54027a04b1a06abedfeb2105e110
Andrey Codez <andreycodez@bixtrix.com>
2022-01-02 04:03:30 +1000

    Placeholders added

--- src/components/UserLogin/UserLogin.jsx
+++ src/components/UserLogin/UserLogin.jsx
@@ -5,8 +5,8 @@ const UserLogin = () => {
       <div className="center-display">
         <form className="form">
           <label htmlFor="credentials">Enter you <strong>email address</strong> and <strong>password</strong></label>
-          <input id="credentials" type="email" className="form-control" name="email"/>
-          <input id="credentials" type="password" className="form-control" name="password" />
+          <input id="credentials" type="email" className="form-control" name="email" placeholder="elonmask@tesla.com"/>
+          <input id="credentials" type="password" className="form-control" name="password" placeholder="password" />
           <input type="submit" className="submit-btn" value="Sign in" />
         </form>
         <div className="footer-text">
