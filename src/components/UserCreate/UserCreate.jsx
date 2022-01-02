0c42a1616df36eb17d87be9db343e6dfa5326460
Andrey Codez <andreycodez@bixtrix.com>
2022-01-02 23:25:29 +1000

    Choose Avatar modal added to the component

--- src/components/UserCreate/UserCreate.jsx
+++ src/components/UserCreate/UserCreate.jsx
@@ -1,26 +1,33 @@
 import {Link} from "react-router-dom";
 import './UserCreate.css';
+import Modal from "../Modal/Modal";
 
 const UserCreate = () => {
   return(
-      <div className="center-display">
-        <h3 className="title">Create an account</h3>
-        <form className="form">
-          <input type="text" className="form-control" name="username" placeholder="enter user name"/>
-          <input type="email" className="form-control" name="email" placeholder="enter email"/>
-          <input type="password" className="form-control" name="password" placeholder="enter password"/>
-          <div className="avatar-container">
-            <img className="avatar-icon avatar-b-radius" src="/avatarDefault.png" alt="avatar"/>
-            <div className="avatar-text">Choose Avatar</div>
-            <div className="avatar-text">Generate background color</div>
+      <>
+        <div className="center-display">
+          <h3 className="title">Create an account</h3>
+          <form className="form">
+            <input type="text" className="form-control" name="username" placeholder="enter user name"/>
+            <input type="email" className="form-control" name="email" placeholder="enter email"/>
+            <input type="password" className="form-control" name="password" placeholder="enter password"/>
+            <div className="avatar-container">
+              <img className="avatar-icon avatar-b-radius" src="/avatarDefault.png" alt="avatar"/>
+              <div className="avatar-text">Choose Avatar</div>
+              <div className="avatar-text">Generate background color</div>
+            </div>
+            <input type="submit" value="Create account" className="submit-btn"/>
+          </form>
+          <div className="footer-text">
+            Already have an account? Login&nbsp;
+            <Link to="/login">Here</Link>
           </div>
-          <input type="submit" value="Create account" className="submit-btn"/>
-        </form>
-        <div className="footer-text">
-          Already have an account? Login&nbsp;
-          <Link to="/login">Here</Link>
         </div>
-      </div>
+        <Modal title="Choose Avatar" isOpen={true}>
+          <div>This is a modal</div>
+        </Modal>
+      </>
+
   )
 }
 
