52ffeb35ad2cae12bb62b4fdca8958038a03cdc1
Andrey Codez <andreycodez@bixtrix.com>
2022-01-04 17:20:51 +1000

    Redirects for Secure pages added

--- src/App.js
+++ src/App.js
@@ -1,50 +1,61 @@
-import logo from './logo.svg';
+import React, { useState, createContext, useContext } from "react";
 import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Redirect
 } from "react-router-dom";
 import ChatApp from "./components/ChatApp/ChatApp";
 import UserLogin from "./components/UserLogin/UserLogin";
 import UserCreate from "./components/UserCreate/UserCreate";
 
+export const UserContext = createContext();
+const AuthProvider = ({ children }) => {
+  const context = {
+    // authService
+    // messageService
+    appSelectedChannel: {},
+    appSetChannel: (ch) => {
+      setAuthContext({...authContext, appSelectedChannel: ch});
+      // update messageService selectedChannel,
+    }
+  }
+
+  const [authContext, setAuthContext] = useState(context);
+
+  return (
+      <UserContext.Provider value={authContext}>
+        { children }
+      </UserContext.Provider>
+  )
+}
+
+const PrivateRoute = ({ children, ...props }) => {
+  const isLoggedIn = false;
+  return (
+      <Route {...props} render={({ location }) => isLoggedIn
+        ? (children)
+        : (<Redirect to={{ pathname: '/login', state: { from: location }}} />)
+      }
+      />
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/register">Register</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+      <AuthProvider>
+        <Router>
           <Switch>
-            <Route path="/about" exact>
-              <ChatApp />
-            </Route>
-            <Route path="/login" exact>
-              <UserLogin />
-            </Route>
-            {/*{A shorter way to make a rout}*/}
+            <Route path="/login" exact component={UserLogin} />
             <Route path="/register" exact component={UserCreate} />
+            <PrivateRoute>
+              <ChatApp />
+            </PrivateRoute>
           </Switch>
-        </div>
-      </Router>
-    </div>
+        </Router>
+      </AuthProvider>
   );
 }
 
