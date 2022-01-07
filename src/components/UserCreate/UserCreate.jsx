cfa4e66672f94e2980fe0a01a14070cc7f4e01f1
Andrey Codez <andreycodez@bixtrix.com>
2022-01-07 17:11:22 +1000

    Registration complete

--- src/components/UserCreate/UserCreate.jsx
+++ src/components/UserCreate/UserCreate.jsx
@@ -4,11 +4,13 @@ import './UserCreate.css';
 import Modal from "../Modal/Modal";
 import {AVATARS} from "../../constants";
 import {UserContext} from "../../App";
+import {logDOM} from "@testing-library/react";
+import Alert from "../Alert/Alert";
 
-const UserCreate = () => {
+const UserCreate = ({ history }) => {
   const { authService } = useContext(UserContext);
   const INIT_STATE = {
-    username: '',
+    userName: '',
     email: '',
     password: '',
     avatarName: 'avatarDefault.png',
@@ -16,6 +18,8 @@ const UserCreate = () => {
   }
   const [userInfo, setUserInfo] = useState(INIT_STATE);
   const [modal, setModal] = useState(false);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onChange = ({ target: {name, value}}) => {
     setUserInfo( { ...userInfo, [name]: value } )
@@ -31,19 +35,51 @@ const UserCreate = () => {
     setUserInfo({...userInfo, avatarColor: `#${randomColor}`});
   }
 
+  const createUser = (e) => {
+    e.preventDefault();
+    console.log("Submit tapped");
+    console.log(userInfo);
+    const { userName, email, password, avatarName, avatarColor } = userInfo;
+    console.log(userName, email, password, avatarName, avatarColor);
+    if (userName && email && password) {
+      setIsLoading(true);
+      authService.registerUser(email, password).then(() => {
+        authService.loginUser(email, password).then(() => {
+          authService.createUser(userName, email, avatarName, avatarColor).then(() => {
+            setUserInfo(INIT_STATE);
+            history.push('/');
+          }).catch((e) => {
+            console.log('Creating user', e);
+            setError(e);
+          })
+        }).catch((e) => {
+          console.log('Loging user', e);
+          setError(e);
+        }).catch((e) => {
+        console.log('Registering user', e);
+        setError(e);
+      })
+      setIsLoading(false);
+      })
+    }
+  }
+
   const { userName, email, password, avatarName, avatarColor } = userInfo;
+  const errorMsg = 'Error creating account. Please try again.'
 
   return(
       <>
         <div className="center-display">
+          {error ? <Alert message={errorMsg} type="alert-danger"/> : null}
+          {isLoading ? <div>Loading...</div> : null}
           <h3 className="title">Create an account</h3>
-          <form className="form">
+          <form onSubmit={createUser} className="form">
             <input
                 type="text"
                 onChange={onChange}
                 value={userName}
                 className="form-control"
-                name="username"
+                name="userName"
                 placeholder="enter user name"/>
             <input
                 type="email"
