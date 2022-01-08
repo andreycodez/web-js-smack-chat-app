728d133f387d19a0678ce5c6ea60443a20f44a9f
Andrey Codez <andreycodez@bixtrix.com>
2022-01-08 17:24:06 +1000

    Chat app screen nav bar added with styles

--- src/components/ChatApp/ChatApp.jsx
+++ src/components/ChatApp/ChatApp.jsx
@@ -1,6 +1,22 @@
+import React, {useContext} from "react";
+import {UserContext} from "../../App";
+import './ChatApp.css';
+
 const ChatApp = () => {
+  const { authService } = useContext(UserContext);
   return(
-      <>ChatApp page</>
+      <div className="chat-app">
+        <nav>
+          <h1>Smack Chat</h1>
+          <div className="user-avatar">
+            <img
+                style={{backgroundColor: authService.avatarColor}}
+                src={authService.avatarName}
+                alt="avatar"/>
+            <div>{authService.name}</div>
+          </div>
+        </nav>
+      </div>
   )
 }
 export default ChatApp;
