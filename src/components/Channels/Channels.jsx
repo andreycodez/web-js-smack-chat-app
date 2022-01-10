8d74b836694e30dfe485f669be56cd216b6db91f
Andrey Codez <andreycodez@bixtrix.com>
2022-01-10 23:45:53 +1000

    Channels component created

---
+++ src/components/Channels/Channels.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Channels.css';
+
+const Channels = () => {
+
+  return (
+      <div className="channel">
+        <div className="channel-header">
+
+        </div>
+      </div>
+  )
+}
