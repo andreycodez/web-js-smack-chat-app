f7668b7cf6bf4fcee0b1c56b977a85aff0ebaefc
Andrey Codez <andreycodez@bixtrix.com>
2022-01-12 10:43:33 +1000

    Semicolon added

--- src/components/ChatApp/ChatApp.jsx
+++ src/components/ChatApp/ChatApp.jsx
@@ -14,7 +14,7 @@ const ChatApp = () => {
   useEffect(() => {
     socketService.establishConnection();
     return () => socketService.closeConnection();
-  }, [])
+  }, []);
 
   const logoutUser = () => {
     authService.logoutUser();
